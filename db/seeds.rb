840c5ba53090f49510e84a0acacb3c38c89523a1
Joao Reis <joaofranciscoreis13@outlook.com>
2021-11-19 15:04:57 +0000

    working backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 require 'json'
 require 'open-uri'
 
+Bookmark.delete_all
+Movie.delete_all
+List.delete_all
+
 url = 'http://tmdb.lewagon.com/movie/top_rated'
 movie_serialized = URI.open(url).read
 movies = JSON.parse(movie_serialized)
@@ -9,7 +13,7 @@ movies['results'].each do |movie|
   Movie.create!(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: movie["https://image.tmdb.org/t/p/w500#{movie['poster_path'}"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
     rating: movie['vote_average']
   )
 end
